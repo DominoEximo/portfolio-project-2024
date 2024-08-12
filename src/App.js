bbf08f1cf11e563f431ca1848405ce46ad846a54
PappDávid <PappDavid32@gmail.com>
2024-08-12 14:02:45 +0200

    Created basic routing for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './containers/home/index';
+import About from './containers/about/index';
+import Resume from './containers/resume/index';
+import Skills from './containers/skills/index';
+import Portfolio from './containers/portfolio/index';
+import Contact from './containers/contact/index';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* particles js */}
+
+      {/* navbar */}
+
+      {/* main page content */}
+      <Routes>
+
+        <Route path='/' element={<Home/>}/>
+
+        <Route path='/about' element={<About/>}/>
+
+        <Route path='/resume' element={<Resume/>}/>
+
+        <Route path='/skills' element={<Skills/>}/>
+
+        <Route path='/portfolio' element={<Portfolio/>}/>
+
+        <Route path='/contact' element={<Contact/>}/>
+
+
+      </Routes>
     </div>
   );
 }
