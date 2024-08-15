5d6d718f87366ba53c6a8b2845523179c60b19f4
PappDávid <PappDavid32@gmail.com>
2024-08-15 18:43:38 +0200

    Filled in the page header content component

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import {BsInfoCircleFill} from 'react-icons/bs'
+import PageHeaderContent from "../../components/pageHeaderContent/index";
 
 const Contact=() =>{
     return(
-        <div>
-            Contact Page
-        </div>
+        <section id="contact" className="contact">
+            
+            <PageHeaderContent
+            headerText = "My Contact"
+            icon = {<BsInfoCircleFill size={40}/>}
+            />
+
+        </section>
     )
 }
 export default Contact;
--- src/containers/portfolio/index.jsx
+++ src/containers/portfolio/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import {BsInfoCircleFill} from 'react-icons/bs'
+import PageHeaderContent from "../../components/pageHeaderContent/index";
 
 const Portfolio=() =>{
     return(
-        <div>
-            Portfolio Page
-        </div>
+        <section id="portfolio" className="portfolio">
+            
+            <PageHeaderContent
+            headerText = "My Portfolio"
+            icon = {<BsInfoCircleFill size={40}/>}
+            />
+
+        </section>
     )
 }
 export default Portfolio;
